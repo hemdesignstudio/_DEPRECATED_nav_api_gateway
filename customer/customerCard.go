58290f8b35ce4144bac2da40e325c7b885095b2a
ahmednafies <ahmed.nafies@gmail.com>
2019-01-01 18:14:53 +0200

    all customerCardWS fields are added

--- customer/customerCard.go
+++ customer/customerCard.go
@@ -14,20 +14,54 @@ type Response struct {
 }
 
 type CustomerCard struct {
-	No   string `json:"no"`
-	Name string `json:"name"`
+	No                          string `json:"No"`
+	Name                        string `json:"Name"`
+	Address                     string `json:"Address"`
+	Address2                    string `json:"Address_2"`
+	PostCode                    string `json:"Post_Code"`
+	City                        string `json:"City"`
+	CountryRegionCode           string `json:"Country_Region_Code"`
+	PhoneNo                     string `json:"Phone_No"`
+	Contact                     string `json:"Contact"`
+	VatRegistrationNumber       string `json:"VAT_Registration_No"`
+	CustomerPostingGroup        string `json:"Customer_Posting_Group"`
+	GeneralBusinessPostingGroup string `json:"Gen_Bus_Posting_Group"`
+	VatBusinessPostingGroup     string `json:"VAT_Bus_Posting_Group"`
+	SalesTypeCode               string `json:"Global_Dimension_2_Code"`
+	CustomerPriceGroup          string `json:"Customer_Price_Group"`
+	CustomerDiscountGroup       string `json:"Customer_Disc_Group"`
+	PaymentTermsCode            string `json:"Payment_Terms_Code"`
+	CurrencyCode                string `json:"Currency_Code"`
+	LanguageCode                string `json:"Language_Code"`
+	WebEmail                    string `json:"Web_E_Mail"`
+	WebEnabled                  bool   `json:"Web_Customer"`
 }
 
 func CreateCustomerCardType() *graphql.Object {
 	return graphql.NewObject(graphql.ObjectConfig{
 		Name: "CustomerCard",
 		Fields: graphql.Fields{
-			"no": &graphql.Field{
-				Type: graphql.String,
-			},
-			"name": &graphql.Field{
-				Type: graphql.String,
-			},
+			"No":                      &graphql.Field{Type: graphql.String},
+			"Name":                    &graphql.Field{Type: graphql.String},
+			"Address":                 &graphql.Field{Type: graphql.String},
+			"Address_2":               &graphql.Field{Type: graphql.String},
+			"Post_Code":               &graphql.Field{Type: graphql.String},
+			"City":                    &graphql.Field{Type: graphql.String},
+			"Country_Region_Code":     &graphql.Field{Type: graphql.String},
+			"Phone_No":                &graphql.Field{Type: graphql.String},
+			"Contact":                 &graphql.Field{Type: graphql.String},
+			"VAT_Registration_No":     &graphql.Field{Type: graphql.String},
+			"Customer_Posting_Group":  &graphql.Field{Type: graphql.String},
+			"Gen_Bus_Posting_Group":   &graphql.Field{Type: graphql.String},
+			"VAT_Bus_Posting_Group":   &graphql.Field{Type: graphql.String},
+			"Global_Dimension_2_Code": &graphql.Field{Type: graphql.String},
+			"Customer_Price_Group":    &graphql.Field{Type: graphql.String},
+			"Customer_Disc_Group":     &graphql.Field{Type: graphql.String},
+			"Payment_Terms_Code":      &graphql.Field{Type: graphql.String},
+			"Currency_Code":           &graphql.Field{Type: graphql.String},
+			"Language_Code":           &graphql.Field{Type: graphql.String},
+			"Web_E_Mail":              &graphql.Field{Type: graphql.String},
+			"Web_Customer":            &graphql.Field{Type: graphql.Boolean},
 		},
 	})
 }
