cf1175b85eb66636b2a161165b8672eb90820637
ahmednafies <ahmed.nafies@gmail.com>
2019-01-04 11:50:35 +0200

    Filter is used instead of getting items by id

--- assemblybom/assemblyBom.go
+++ assemblybom/assemblyBom.go
@@ -9,6 +9,8 @@ import (
 	"github.com/nav-api-gateway/request"
 )
 
+var conf = config.GetConfig()
+
 type Response struct {
 	Value []AssemblyBom `json:"value"`
 }
@@ -35,7 +37,6 @@ func CreateAssemblyBomType() *graphql.Object {
 }
 
 func GetAssemblyBomByCompanyName(name string) ([]AssemblyBom, error) {
-	conf := config.GetConfig()
 	url := conf.BaseUrl + conf.CompanyEndpoint + fmt.Sprintf("('%s')"+conf.AssemblyBomEndpoint, name)
 	resultByte, err := request.GET(url)
 	res := Response{}
@@ -47,18 +48,12 @@ func GetAssemblyBomByCompanyName(name string) ([]AssemblyBom, error) {
 	return res.Value, nil
 }
 
-func GetAssemblyByNo(companyName string, no string) (*AssemblyBom, error) {
-	conf := config.GetConfig()
-	url := conf.BaseUrl +
-		conf.CompanyEndpoint + fmt.Sprintf("('%s')", companyName) +
-		conf.AssemblyBomEndpoint + fmt.Sprintf("('%s')", no)
-
-	resultByte, err := request.GET(url)
-	response := AssemblyBom{}
-	err = json.Unmarshal(resultByte, &response)
-
+func GetAssemblyBomByFilter(companyName string, args map[string]interface{}) ([]AssemblyBom, error) {
+	resByte := request.Filter(companyName, conf.AssemblyBomEndpoint, args)
+	res := Response{}
+	err := json.Unmarshal(resByte, &res)
 	if err != nil {
 		return nil, errors.New("could not unmarshal data")
 	}
-	return &response, nil
+	return res.Value, nil
 }
