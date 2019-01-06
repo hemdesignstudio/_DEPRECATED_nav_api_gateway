e183484851346dad4a8cb9337f94ee720d60bf0f
ahmednafies <ahmed.nafies@gmail.com>
2019-01-06 15:21:12 +0200

    arguments are moved to a separate map

--- company/fields.go
+++ company/fields.go
@@ -20,11 +20,16 @@ var types = map[string]*graphql.Object{
 	"postShip":    postship.CreateType(),
 }
 
+var filterArgs = map[string]*graphql.ArgumentConfig{
+	"key":   {Type: graphql.String},
+	"value": {Type: graphql.String},
+}
+
 func getCompanyFields() map[string]*graphql.Field {
 	fields := map[string]*graphql.Field{
-		"id":          &graphql.Field{Type: graphql.String},
-		"name":        &graphql.Field{Type: graphql.String},
-		"displayName": &graphql.Field{Type: graphql.String},
+		"id":          {Type: graphql.String},
+		"name":        {Type: graphql.String},
+		"displayName": {Type: graphql.String},
 	}
 	return fields
 }
@@ -32,10 +37,7 @@ func getCompanyFields() map[string]*graphql.Field {
 func getAssemblyBomFields() *graphql.Field {
 	field := &graphql.Field{
 		Type: graphql.NewList(types["assemblyBom"]),
-		Args: graphql.FieldConfigArgument{
-			"key":   &graphql.ArgumentConfig{Type: graphql.String},
-			"value": &graphql.ArgumentConfig{Type: graphql.String},
-		},
+		Args: filterArgs,
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 			company, _ := p.Source.(*Company)
 			log.Printf("fetching Assembly BOM of company: %s", company.Name)
@@ -51,10 +53,7 @@ func getAssemblyBomFields() *graphql.Field {
 func getCustomerCardFields() *graphql.Field {
 	field := &graphql.Field{
 		Type: graphql.NewList(types["customer"]),
-		Args: graphql.FieldConfigArgument{
-			"key":   &graphql.ArgumentConfig{Type: graphql.String},
-			"value": &graphql.ArgumentConfig{Type: graphql.String},
-		},
+		Args: filterArgs,
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 			company, _ := p.Source.(*Company)
 			log.Printf("fetching Customer cards of company: %s", company.Name)
@@ -70,10 +69,7 @@ func getCustomerCardFields() *graphql.Field {
 func getItemCardFields() *graphql.Field {
 	field := &graphql.Field{
 		Type: graphql.NewList(types["item"]),
-		Args: graphql.FieldConfigArgument{
-			"key":   &graphql.ArgumentConfig{Type: graphql.String},
-			"value": &graphql.ArgumentConfig{Type: graphql.String},
-		},
+		Args: filterArgs,
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 			company, _ := p.Source.(*Company)
 			log.Printf("fetching Items of company: %s", company.Name)
@@ -89,10 +85,7 @@ func getItemCardFields() *graphql.Field {
 func getSalesOrdersFields() *graphql.Field {
 	field := &graphql.Field{
 		Type: graphql.NewList(types["salesOrder"]),
-		Args: graphql.FieldConfigArgument{
-			"key":   &graphql.ArgumentConfig{Type: graphql.String},
-			"value": &graphql.ArgumentConfig{Type: graphql.String},
-		},
+		Args: filterArgs,
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 			company, _ := p.Source.(*Company)
 			log.Printf("fetching sales orders of company: %s", company.Name)
@@ -108,10 +101,7 @@ func getSalesOrdersFields() *graphql.Field {
 func getSalesLinesFields() *graphql.Field {
 	field := &graphql.Field{
 		Type: graphql.NewList(types["salesLine"]),
-		Args: graphql.FieldConfigArgument{
-			"key":   &graphql.ArgumentConfig{Type: graphql.String},
-			"value": &graphql.ArgumentConfig{Type: graphql.String},
-		},
+		Args: filterArgs,
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 			company, _ := p.Source.(*Company)
 			log.Printf("fetching sales lines of company: %s", company.Name)
@@ -128,10 +118,7 @@ func getSalesLinesFields() *graphql.Field {
 func getPostShipFields() *graphql.Field {
 	field := &graphql.Field{
 		Type: graphql.NewList(types["postShip"]),
-		Args: graphql.FieldConfigArgument{
-			"key":   &graphql.ArgumentConfig{Type: graphql.String},
-			"value": &graphql.ArgumentConfig{Type: graphql.String},
-		},
+		Args: filterArgs,
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 			company, _ := p.Source.(*Company)
 			log.Printf("fetching Posted sales shipments of company: %s", company.Name)
@@ -141,6 +128,5 @@ func getPostShipFields() *graphql.Field {
 			return postship.Filter(company.Name, p.Args)
 		},
 	}
-
 	return field
 }
