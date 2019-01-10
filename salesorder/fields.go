e500bd09950388aa3b2e4ec55ac025734824ff8e
ahmednafies <ahmed.nafies@gmail.com>
2019-01-10 17:05:30 +0200

    sales lines field for sales order is restored.

--- salesorder/fields.go
+++ salesorder/fields.go
@@ -2,27 +2,26 @@ package salesorder
 
 import (
 	"github.com/graphql-go/graphql"
+	"github.com/nav-api-gateway/config"
 	"github.com/nav-api-gateway/salesline"
+	"log"
 )
 
 var types = map[string]*graphql.Object{
 	"salesLine": salesline.CreateSalesLineType(),
 }
 
-/*
 func getSalesLinesFields() *graphql.Field {
 	field := &graphql.Field{
 		Type: graphql.NewList(types["salesLine"]),
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-			company, _ := p.Source.(*company.Company)
-			log.Printf("fetching sales lines of company: %s", company.Name)
-			if len(p.Args) != 2 {
-				return salesline.GetSalesLineByCompanyName(company.Name)
-			}
-
-			return salesline.GetSalesLineByFilter(company.Name, p.Args)
+			log.Printf("fetching sales lines of company: %s", config.CompanyName)
+			salesOrder, _ := p.Source.(SalesOrder)
+			print(salesOrder.No)
+			p.Args["key"] = "Document_No"
+			p.Args["value"] = salesOrder.No
+			return salesline.Filter(p.Args)
 		},
 	}
 	return field
 }
-*/
