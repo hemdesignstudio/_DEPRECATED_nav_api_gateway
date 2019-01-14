4e3c721e065094f9ed21ad0dbaf8326f6cf06bca
ahmednafies <ahmed.nafies@gmail.com>
2019-01-14 16:54:16 +0200

    sales line field is added to sales invoice

---
+++ salesinvoice/fields.go
@@ -0,0 +1,27 @@
+package salesinvoice
+
+import (
+	"github.com/graphql-go/graphql"
+	"github.com/nav-api-gateway/config"
+	"github.com/nav-api-gateway/salesline"
+	"log"
+)
+
+var types = map[string]*graphql.Object{
+	"salesLine": salesline.CreateSalesLineType("Invoice_SalesLines"),
+}
+
+func getSalesLinesFields() *graphql.Field {
+	field := &graphql.Field{
+		Type: graphql.NewList(types["salesLine"]),
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			log.Printf("fetching sales lines of company: %s", config.CompanyName)
+			salesInvoice, _ := p.Source.(SalesInvoice)
+			print(salesInvoice.No)
+			p.Args["key"] = "Document_No"
+			p.Args["value"] = salesInvoice.No
+			return salesline.Filter(p.Args)
+		},
+	}
+	return field
+}
