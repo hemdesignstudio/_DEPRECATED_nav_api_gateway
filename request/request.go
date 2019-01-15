36c6da9855f8c539d7e024dfe2b5e28caba868f8
ahmednafies <ahmed.nafies@gmail.com>
2019-01-15 13:15:27 +0200

    Function for POST Requests is added.

--- request/request.go
+++ request/request.go
@@ -38,6 +38,36 @@ func GET(uri string) ([]byte, error) {
 	return resultByte, nil
 }
 
+func POST(uri string, body []byte) ([]byte, error) {
+	u, err := url.Parse(uri)
+	if err != nil {
+		panic(err)
+	}
+	u.RawQuery = u.Query().Encode()
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", u.String(), bytes.NewBuffer(body))
+	req.SetBasicAuth(config.Username, config.Passwd)
+	req.Header.Add("If-Match", "*")
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json; odata.metadata=minimal")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 201 {
+		return nil, fmt.Errorf("%s: %s", "could not Post data", resp.Status)
+	}
+	resultByte, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.New("could not read data")
+	}
+	return resultByte, nil
+}
+
 func PATCH(uri string, body []byte) (string, error) {
 	u, err := url.Parse(uri)
 	if err != nil {
@@ -59,7 +89,7 @@ func PATCH(uri string, body []byte) (string, error) {
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
-		return "", fmt.Errorf("%s: %s", "could not fetch data", resp.Status)
+		return "", fmt.Errorf("%s: %s", "could not update data", resp.Status)
 	}
 
 	return resp.Status, nil
@@ -92,6 +122,17 @@ func Filter(companyName, endpoint string, args map[string]interface{}) []byte {
 
 	resultByte, _ := GET(uri + filter)
 	return resultByte
+}
+
+func Create(companyName string, endpoint string, body []byte) []byte {
+	uri := config.BaseUrl +
+		config.CompanyEndpoint +
+		fmt.Sprintf("('%s')", companyName) +
+		endpoint
+
+	resultByte, _ := POST(uri, body)
+	return resultByte
+
 }
 
 func Update(companyName string, endpoint string, id string, body []byte) string {
