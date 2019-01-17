d12f7f013ec5c0c4dcd72f38184f4b0321fa1c1a
ahmednafies <ahmed.nafies@gmail.com>
2019-01-17 12:38:41 +0200

    Error handelling for GET requests is added to the Sales Order module

--- salesorder/salesOrder.go
+++ salesorder/salesOrder.go
@@ -139,7 +139,10 @@ func GetAll() ([]SalesOrder, error) {
 }
 
 func Filter(args map[string]interface{}) ([]SalesOrder, error) {
-	resByte := request.Filter(companyName, endpoint, args)
+	resByte, resError := request.Filter(companyName, endpoint, args)
+	if resError != nil {
+		return nil, resError
+	}
 	res := Response{}
 	err := json.Unmarshal(resByte, &res)
 	if err != nil {
