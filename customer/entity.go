8fed2daa7fef572f9a50d6e2c8fa4e5150807418
ahmednafies <ahmed.nafies@gmail.com>
2019-01-17 23:36:16 +0200

    customer card entity is added

---
+++ customer/entity.go
@@ -0,0 +1,36 @@
+package customer
+
+import (
+	"github.com/nav-api-gateway/config"
+)
+
+var endpoint = config.CustomerCardWSEndpoint
+var companyName = config.CompanyName
+
+type Response struct {
+	Value []CustomerCard `json:"value"`
+}
+
+type CustomerCard struct {
+	No                          string `json:"No"`
+	Name                        string `json:"Name"`
+	Address                     string `json:"Address"`
+	Address2                    string `json:"Address_2"`
+	PostCode                    string `json:"Post_Code"`
+	City                        string `json:"City"`
+	CountryRegionCode           string `json:"Country_Region_Code"`
+	PhoneNo                     string `json:"Phone_No"`
+	Contact                     string `json:"Contact"`
+	VatRegistrationNumber       string `json:"VAT_Registration_No"`
+	CustomerPostingGroup        string `json:"Customer_Posting_Group"`
+	GeneralBusinessPostingGroup string `json:"Gen_Bus_Posting_Group"`
+	VatBusinessPostingGroup     string `json:"VAT_Bus_Posting_Group"`
+	SalesTypeCode               string `json:"Global_Dimension_2_Code"`
+	CustomerPriceGroup          string `json:"Customer_Price_Group"`
+	CustomerDiscountGroup       string `json:"Customer_Disc_Group"`
+	PaymentTermsCode            string `json:"Payment_Terms_Code"`
+	CurrencyCode                string `json:"Currency_Code"`
+	LanguageCode                string `json:"Language_Code"`
+	WebEmail                    string `json:"Web_E_Mail"`
+	WebEnabled                  bool   `json:"Web_Customer"`
+}
