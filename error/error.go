3ffa096f1a9b0c6780d632399457b2d73e4263d0
ahmednafies <ahmed.nafies@gmail.com>
2019-01-17 13:06:20 +0200

    Separate Module for Error handling is created

---
+++ error/error.go
@@ -0,0 +1,42 @@
+package error
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Handler struct {
+	Content Content `json:"error"`
+}
+
+type Content struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+func ErrorOccuredButNotUnmarshaled() error {
+	return fmt.Errorf("error occured, however Gateway API has failed to unmarshal the error Response from NAV")
+}
+
+func BadRequest(statusCode int, handler Handler) error {
+	return fmt.Errorf("status: %d, %s, %s", statusCode, handler.Content.Code, handler.Content.Message)
+}
+
+func UnknownError() error {
+	return fmt.Errorf("there has been an unknown urror which has occured")
+}
+
+func Handle(statusCode int, errBody []byte) error {
+	handler := Handler{}
+	err := json.Unmarshal(errBody, &handler)
+	if err != nil {
+		return ErrorOccuredButNotUnmarshaled()
+	}
+
+	if statusCode == http.StatusBadRequest {
+		return BadRequest(statusCode, handler)
+	}
+
+	return UnknownError()
+}
