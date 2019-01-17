29055272cf9bee5efbfbffe757da7aae335e9db5
ahmednafies <ahmed.nafies@gmail.com>
2019-01-17 23:37:40 +0200

    Item entity is added

---
+++ item/entity.go
@@ -0,0 +1,36 @@
+package item
+
+import (
+	"github.com/nav-api-gateway/config"
+)
+
+var endpoint = config.ItemCardEndpoint
+var companyName = config.CompanyName
+
+type Response struct {
+	Value []ItemCard `json:"value"`
+}
+
+type ItemCard struct {
+	No                        string  `json:"No"`
+	Description               string  `json:"Description"`
+	BaseUnitOfMeasure         string  `json:"Base_Unit_of_Measure"`
+	AssemblyBOM               bool    `json:"Assembly_BOM"`
+	ItemCategoryCode          string  `json:"Item_Category_Code"`
+	ProductGroupCode          string  `json:"Product_Group_Code"`
+	Inventory                 int     `json:"Inventory"`
+	QtyOnPurchOrder           int     `json:"Qty_on_Purch_Order"`
+	QtyOnSalesOrder           int     `json:"Qty_on_Sales_Order"`
+	LastDateModified          string  `json:"Last_Date_Modified"`
+	FreightType               string  `json:"Freight_Type"`
+	AssemblyPolicy            string  `json:"Assembly_Policy"`
+	CountryRegionOfOriginCode string  `json:"Country_Region_of_Origin_Code"`
+	NetWeight                 float32 `json:"Net_Weight"`
+	GrossWeight               float32 `json:"Gross_Weight"`
+	UnitVolume                float32 `json:"Unit_Volume"`
+	Length                    float32 `json:"Length"`
+	Width                     float32 `json:"Width"`
+	Height                    float32 `json:"Height"`
+	Designer                  string  `json:"Designer"`
+	WebStatus                 string  `json:"Web_Status"`
+}
