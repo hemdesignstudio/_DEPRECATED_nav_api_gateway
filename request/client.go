870cd346be52443931feb457aade2f02da06feb6
ahmednafies <ahmed.nafies@gmail.com>
2019-01-18 10:43:07 +0200

    client function order is modified

--- request/client.go
+++ request/client.go
@@ -9,6 +9,21 @@ import (
 	"net/url"
 )
 
+func headers(uri string, method string, body []byte) *http.Request {
+	u, err := url.Parse(uri)
+	if err != nil {
+		panic(err)
+	}
+	u.RawQuery = u.Query().Encode()
+	req, err := http.NewRequest(method, u.String(), bytes.NewBuffer(body))
+	req.SetBasicAuth(config.Username, config.Passwd)
+	req.Header.Add("If-Match", "*")
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json; odata.metadata=minimal")
+	return req
+
+}
+
 func clientRequest(req *http.Request, method string) ([]byte, error) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -29,21 +44,6 @@ func clientRequest(req *http.Request, method string) ([]byte, error) {
 		}
 	}
 	return respBody, nil
-}
-
-func headers(uri string, method string, body []byte) *http.Request {
-	u, err := url.Parse(uri)
-	if err != nil {
-		panic(err)
-	}
-	u.RawQuery = u.Query().Encode()
-	req, err := http.NewRequest(method, u.String(), bytes.NewBuffer(body))
-	req.SetBasicAuth(config.Username, config.Passwd)
-	req.Header.Add("If-Match", "*")
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/json; odata.metadata=minimal")
-	return req
-
 }
 
 func request(uri string, method string, body []byte) ([]byte, error) {
