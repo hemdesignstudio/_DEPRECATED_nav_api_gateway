02043387ad78a690b45940704e6e21717e07ee93
ahmednafies <ahmed.nafies@gmail.com>
2019-01-31 19:26:46 +0200

    serializer function is added to utils

--- test/utils.go
+++ test/utils.go
@@ -2,8 +2,19 @@ package test
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 )
+
+func serialize(element interface{}) []interface{} {
+
+	elemObj := reflect.ValueOf(element)
+	elemValues := make([]interface{}, elemObj.NumField())
+	for i := 0; i < elemObj.NumField(); i++ {
+		elemValues[i] = elemObj.Field(i).Interface()
+	}
+	return elemValues
+}
 
 func getAllQuery(page string, attrs []string) string {
 	attrStr := strings.Join(attrs, " ")
