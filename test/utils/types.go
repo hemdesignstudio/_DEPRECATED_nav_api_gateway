c9fb4d4b392e62d6ccc321aedbe001ac4574aabc
ahmednafies <ahmed.nafies@gmail.com>
2019-02-04 17:35:36 +0200

    Graphql errors and Item is added to utils.types

--- test/utils/types.go
+++ test/utils/types.go
@@ -3,22 +3,34 @@ package utils
 type SliceOfMaps []map[string]interface{}
 type MapOfSliceOfMaps map[string][]map[string]interface{}
 
+type ErrorMessage struct {
+	Message interface{} `json:"message"`
+}
+
 type QueryType struct {
 	AssemblyBom  string
 	CustomerCard string
+	ItemCard     string
 }
 
 type MutationType struct {
 	CreateCustomerCard string
+	CreateItemCard     string
+
 	UpdateCustomerCard string
+	UpdateItemCard     string
 }
 
 var Query = QueryType{
 	AssemblyBom:  "AssemblyBom",
 	CustomerCard: "CustomerCard",
+	ItemCard:     "ItemCard",
 }
 
 var Mutation = MutationType{
 	CreateCustomerCard: "CreateCustomerCard",
 	UpdateCustomerCard: "UpdateCustomerCard",
+
+	CreateItemCard: "CreateItemCard",
+	UpdateItemCard: "UpdateItemCard",
 }
