434a783b99856e45bfb0fc829e054112d102652f
ahmednafies <ahmed.nafies@gmail.com>
2019-02-08 16:52:43 +0200

    message is corrected

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/nav-api-gateway/config"
 	"github.com/nav-api-gateway/roothandler"
+
 	"log"
 	"net/http"
 )
@@ -14,7 +15,7 @@ func main() {
 	handler := roothandler.RootEndpoint
 	router.HandleFunc(config.Endpoint+config.Version+"/{company:[a-zA-Z]+}", handler)
 
-	fmt.Println("Server started at http://localhost:6789/graphql/v0.1.0")
+	fmt.Println("Server started at http://localhost:6789/graphql/v0.1.0/test")
 	http.Handle("/", router)
 	log.Fatal(http.ListenAndServe(config.Host, nil))
 
