885d1b40e15aba5ab0ce5305c08b7703da19ba6d
ahmednafies <ahmed.nafies@gmail.com>
2019-02-08 13:07:15 +0200

    test client is now using the graphql handler directly

--- test/utils/client.go
+++ test/utils/client.go
@@ -13,7 +13,7 @@ func Client(method, query string, body interface{}) (int, []byte) {
 	jsonBody, _ := json.Marshal(body)
 	request, _ := http.NewRequest(method, query, bytes.NewBuffer(jsonBody))
 	response := httptest.NewRecorder()
-	handler := roothandler.RootEndpoint()
+	handler := roothandler.Handler()
 	handler.ServeHTTP(response, request)
 	resBody, _ := ioutil.ReadAll(response.Body)
 	return response.Code, resBody
