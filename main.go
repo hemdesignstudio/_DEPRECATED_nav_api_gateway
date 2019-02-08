d026a0dc4d92930cac20772c764f33d9164ef6f8
ahmednafies <ahmed.nafies@gmail.com>
2019-02-08 19:22:27 +0200

    doc hosting is added to router

--- main.go
+++ main.go
@@ -5,17 +5,20 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/nav-api-gateway/config"
 	"github.com/nav-api-gateway/roothandler"
-
 	"log"
 	"net/http"
 )
 
 func main() {
-	router := mux.NewRouter()
+	path := config.Endpoint + config.Version
+	router := mux.NewRouter().StrictSlash(true)
 	handler := roothandler.RootEndpoint
-	router.HandleFunc(config.Endpoint+config.Version+"/{company:[a-zA-Z]+}", handler)
+	fs := http.FileServer(http.Dir("./doc/"))
+	router.PathPrefix(path + "/doc/").Handler(http.StripPrefix(path+"/doc/", fs))
 
-	fmt.Println("Server started at http://localhost:6789/graphql/v0.1.0/test")
+	router.HandleFunc(path+"/company={company:[a-zA-Z]+}", handler)
+
+	fmt.Println("Server started at http://localhost:6789/graphql/v0.1.0/company=test")
 	http.Handle("/", router)
 	log.Fatal(http.ListenAndServe(config.Host, nil))
 
