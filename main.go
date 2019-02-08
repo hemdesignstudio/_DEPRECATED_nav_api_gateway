6842149060348faf8e31980fa2b51a493a6b2fb4
ahmednafies <ahmed.nafies@gmail.com>
2019-02-08 13:06:50 +0200

    router is added to handle path variables

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/nav-api-gateway/config"
 	"github.com/nav-api-gateway/roothandler"
 	"log"
@@ -9,7 +10,12 @@ import (
 )
 
 func main() {
-	http.Handle(config.Endpoint+config.Version, roothandler.RootEndpoint())
+	router := mux.NewRouter()
+	handler := roothandler.RootEndpoint
+	router.HandleFunc(config.Endpoint+config.Version+"/{company:[a-zA-Z]+}", handler)
+
 	fmt.Println("Server started at http://localhost:6789/graphql/v0.1.0")
+	http.Handle("/", router)
 	log.Fatal(http.ListenAndServe(config.Host, nil))
+
 }
