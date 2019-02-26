21a01e866849f9f2a7cecf5e815c707a29889430
ahmednafies <ahmed.nafies@gmail.com>
2019-02-26 21:32:10 +0200

    utils is moved to fields

--- utils/fields.go
+++ utils/fields.go
@@ -2,13 +2,62 @@ package utils
 
 import (
 	"github.com/graphql-go/graphql"
+	"github.com/nav-api-gateway/assemblybom"
+	"github.com/nav-api-gateway/customer"
+	"github.com/nav-api-gateway/item"
+	"github.com/nav-api-gateway/postship"
+	"github.com/nav-api-gateway/salesinvoice"
+	"github.com/nav-api-gateway/salesline"
+	"github.com/nav-api-gateway/salesorder"
 )
 
-type Args map[string]*graphql.ArgumentConfig
 type getAllFunction func() (interface{}, error)
 type filterFunction func(map[string]interface{}) (interface{}, error)
 type createFunction func(map[string]interface{}) (interface{}, error)
 type UpdateFunction func(map[string]interface{}) (interface{}, error)
+
+var types = map[string]*graphql.Object{
+	"customer":     customer.CreateType(),
+	"assemblyBom":  assemblybom.CreateType(),
+	"item":         item.CreateType(),
+	"salesOrder":   salesorder.CreateType(),
+	"salesLine":    salesline.CreateType("SalesLine"),
+	"postShip":     postship.CreateType(),
+	"salesInvoice": salesinvoice.CreateType(),
+}
+
+func QueryType() *graphql.Object {
+	return graphql.NewObject(graphql.ObjectConfig{
+		Name: "RootQuery",
+		Fields: graphql.Fields{
+			"AssemblyBom":         queryFields("assemblyBom", assemblybom.GetAll, assemblybom.Filter),
+			"CustomerCard":        queryFields("customer", customer.GetAll, customer.Filter),
+			"ItemCard":            queryFields("item", item.GetAll, item.Filter),
+			"SalesOrder":          queryFields("salesOrder", salesorder.GetAll, salesorder.Filter),
+			"SalesLine":           queryFields("salesLine", salesline.GetAll, salesline.Filter),
+			"PostedSalesShipment": queryFields("postShip", postship.GetAll, postship.Filter),
+			"SalesInvoice":        queryFields("salesInvoice", salesinvoice.GetAll, salesinvoice.Filter),
+		},
+	})
+}
+
+func MutationType() *graphql.Object {
+	return graphql.NewObject(graphql.ObjectConfig{
+		Name: "RootMutation",
+		Fields: graphql.Fields{
+			"CreateCustomerCard": createFields("customer", CustomerCardArgs, customer.Create),
+			"CreateItemCard":     createFields("item", itemCardArgs, item.Create),
+			"CreateSalesOrder":   createFields("salesOrder", salesOrderArgs, salesorder.Create),
+			"CreateSalesLine":    createFields("salesLine", createSalesLineArgs(), salesline.Create),
+			"CreateSalesInvoice": createFields("salesInvoice", salesInvoiceArgs(), salesinvoice.Create),
+			"UpdateCustomerCard": updateFields("customer", CustomerCardArgs, customer.Update),
+			"UpdateItemCard":     updateFields("item", itemCardArgs, item.Update),
+			"UpdateSalesOrder":   updateFields("salesOrder", salesOrderArgs, salesorder.Update),
+			"UpdateSalesLine":    updateFields("salesLine", updateSalesLineArgs(), salesline.Update),
+			"UpdateSalesInvoice": updateFields("salesInvoice", updateSalesInvoiceArgs(), salesinvoice.Create),
+		},
+	})
+}
 
 func queryFields(name string, getAll getAllFunction, filter filterFunction) *graphql.Field {
 	field := &graphql.Field{
