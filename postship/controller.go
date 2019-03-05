270b7b6792480b43999b2f3d8cee6fee189cf199
ahmednafies <ahmed.nafies@gmail.com>
2019-03-05 14:40:38 +0200

    documentation for postship package is added

--- postship/controller.go
+++ postship/controller.go
@@ -1,15 +1,24 @@
+// Copyright 2019 Hem Design Studio. All rights reserved.
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file.
+
 package postship
 
 import (
 	"github.com/hem-nav-gateway/request"
 )
 
+//GetAll retrieves a List of all Posted Sales Shipments available Microsoft Navision .
+//Function takes a list of fields to be returned by Microsoft Navision.
 func GetAll(fields interface{}) (interface{}, error) {
 
 	return request.GetAll(endpoint, fields, Response{})
 
 }
 
+//Filter retrieves a list of filtered Posted Sales Shipments based on a key-value pair added by the requester
+//Function takes a list of fields to be returned by Microsoft Navision.
+//Function takes filter arguments which are required for filtering results in Navision
 func Filter(fields, args interface{}) (interface{}, error) {
 
 	return request.Filter(endpoint, fields, args, Response{})
--- postship/entity.go
+++ postship/entity.go
@@ -1,3 +1,25 @@
+// Copyright 2019 Hem Design Studio. All rights reserved.
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file.
+
+/*
+Package postship implements a simple package for handling all graphql
+operations related to Microsoft Navision PostedSalesShipment page.
+
+Package has a type "PostShip" where all the fields related to WebSalesShipment page are defined.
+
+		type PostShip struct {
+			No                       string `json:"No" required:"true"`
+			SellToCustomerNo         string `json:"Sell_to_Customer_No"`
+			SellToContactNo          string `json:"Sell_to_Contact_No"`
+			...
+		}
+
+
+
+GraphQl Object Type along with its fields, arguments and attributes are generated
+from the PostShip type when "CreateType" method is invoked.
+*/
 package postship
 
 import (
@@ -6,9 +28,38 @@ import (
 	"github.com/hem-nav-gateway/types"
 )
 
+//Microsoft Navision endpoint path for WebSalesShipment page
 var endpoint = config.PostShipEndpoint
-var companyName = config.CompanyName
 
+/*
+Response is utilized as Microsoft Navision returns a list of objects
+when requesting WebSalesShipment, It is utilized for JSON decoding
+
+example response from Navision
+
+		{
+			"value": [
+				{
+					"No": "102001",
+					"Sell_to_Customer_No": "405124",
+					"Sell_to_Contact_No": "",
+					"Sell_to_Customer_Name": "Goodform",
+					...
+				},
+				{
+					"No": "102001",
+					"Sell_to_Customer_No": "405124",
+					"Sell_to_Contact_No": "",
+					"Sell_to_Customer_Name": "Goodform",
+					...
+				},
+				{
+				...
+
+				},
+		}
+
+*/
 type Response struct {
 	Value []PostShip `json:"value"`
 }
@@ -62,6 +113,25 @@ type PostShip struct {
 	ShipmentDate             string `json:"Shipment_Date"`
 }
 
+/*
+CreateType function creates a GraphQl Object Type from the 'PostShip' type.
+
+example of GraphQl Object
+
+		graphql.NewObject(graphql.ObjectConfig{
+				Name: "PostShip",
+				Fields: graphql.Fields{
+					"No":       		   &graphql.Field{Type: graphql.String},
+					"Sell_to_Customer_No": &graphql.Field{Type: graphql.String},
+					"Sell_to_Contact_No":  &graphql.Field{Type: graphql.String},
+					...
+				},
+			})
+
+GraphQl Object is a map[string]*graphql.Field
+
+The returned GraphQl Object Type will be used as a part of the main query
+*/
 func CreateType() *graphql.Object {
 	return types.GenerateGraphQlType("PostShip", PostShip{}, nil)
 }
