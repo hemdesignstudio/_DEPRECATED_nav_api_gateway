bfaf879155ec1b52dd297087ce6618d88f42aaeb
ahmednafies <ahmed.nafies@gmail.com>
2019-03-05 15:27:56 +0200

    Documentation is added to entity and controller of salesinvoice package

--- salesinvoice/controller.go
+++ salesinvoice/controller.go
@@ -8,21 +8,42 @@ import (
 	"github.com/hem-nav-gateway/request"
 )
 
+//GetAll retrieves a List of all SalesInvoices available Microsoft Navision .
+//Function takes a list of fields to be returned by Microsoft Navision.
+//SalesInvoice has nested GraphQL fields SalesLine.
+//SalesLine is removed is it is only known to this API and should not be sent to Navision
 func GetAll(fields interface{}) (interface{}, error) {
 	fields = removeField("Sales_Lines", fields)
 	return request.GetAll(endpoint, fields, Response{})
 }
 
+//Filter retrieves a list of filtered SalesInvoices based on a key-value pair added by the requester
+//Function takes a list of fields to be returned by Microsoft Navision.
+//Function takes filter arguments which are required for filtering results in Navision
+//SalesInvoice has nested GraphQL fields SalesLine.
+//SalesLine is removed is it is only known to this API and should not be sent to Navision
 func Filter(fields, args interface{}) (interface{}, error) {
 	fields = removeField("Sales_Lines", fields)
 	return request.Filter(endpoint, fields, args, Response{})
 }
 
+//Create creates a SalesInvoice objects based on arguments added by the requester
+//Function takes a list of fields to be returned by Microsoft Navision after creation.
+//SalesInvoice has nested GraphQL fields SalesLine.
+//SalesLine is removed is it is only known to this API and should not be sent to Navision
 func Create(fields, args interface{}) (interface{}, error) {
 	fields = removeField("Sales_Lines", fields)
 	return request.Create(endpoint, fields, args, Response{})
 }
 
+//Update modifies a certain SalesInvoice Object Microsoft Navision.
+//Function takes filter arguments which are required identifying
+//the specific object to be updated/modified.
+//Function returns a list of AssemblyBom values
+//For SalesInvoice, Navision requires an extra argument
+//Which Is document Type in this case it is Document_Type = "Invoice"
+//SalesInvoice has nested GraphQL fields SalesLine.
+//SalesLine is removed is it is only known to this API and should not be sent to Navision
 func Update(fields, args interface{}) (interface{}, error) {
 	docType := "Invoice"
 	fields = removeField("Sales_Lines", fields)
--- salesinvoice/entity.go
+++ salesinvoice/entity.go
@@ -2,6 +2,23 @@
 // Use of this source code is governed by a
 // license that can be found in the LICENSE file.
 
+/*
+Package salesinvoice implements a simple package for handling all graphql
+operations related to Microsoft Navision SalesInvoice page.
+
+Package has a type "SalesInvoice" where all the fields related to SalesInvoice are defined.
+
+	type SalesInvoice struct {
+		No                       string                `json:"No"`
+		SellToCustomerNo         string                `json:"Sell_to_Customer_No"`
+		SellToContactNo          string                `json:"Sell_to_Contact_No"`
+		...
+	}
+
+
+GraphQl Object Type along with its fields, arguments and attributes are generated
+from the SalesInvoice type when "CreateType" method is invoked.
+*/
 package salesinvoice
 
 import (
@@ -11,9 +28,38 @@ import (
 	"github.com/hem-nav-gateway/types"
 )
 
+//Microsoft Navision endpoint path for SalesInvoice page
 var endpoint = config.SalesInvoiceEndpoint
-var companyName = config.CompanyName
 
+/*
+Response is utilized as Microsoft Navision returns a list of objects
+when requesting SalesInvoice, It is utilized for JSON decoding
+
+example response from Navision
+
+	{
+		"value": [
+			{
+				"No": "100001",
+				"Sell_to_Customer_No": "206901",
+				"Sell_to_Customer_Name": "Studio Feuer AB",
+
+				...
+			},
+			{
+				"No": "100002",
+				"Sell_to_Customer_No": "WEB-10",
+				"Sell_to_Customer_Name": "Web Customer Currency EUR",
+
+				...
+			},
+			{
+			...
+
+			},
+	}
+
+*/
 type Response struct {
 	Value []SalesInvoice `json:"value"`
 }
@@ -79,10 +125,56 @@ type SalesInvoice struct {
 	SalesLines               []salesline.SalesLine `json:"Sales_Lines"`
 }
 
+/*
+CreateType function creates a GraphQl Object Type from the 'SalesInvoice' type.
+
+example of GraphQl Object
+
+	graphql.NewObject(graphql.ObjectConfig{
+			Name: "SalesInvoice",
+			Fields: graphql.Fields{
+				"No":					&graphql.Field{Type: graphql.String},
+				"Sell_to_Customer_No":	&graphql.Field{Type: graphql.String},
+				"Sell_to_Customer_Name":&graphql.Field{Type: graphql.String},
+				...
+			},
+		})
+
+GraphQl Object is a map[string]*graphql.Field
+
+The returned GraphQl Object Type will be used as a part of the main query
+*/
 func CreateType() *graphql.Object {
 	return types.GenerateGraphQlType("SalesInvoice", SalesInvoice{}, extraFields())
 }
 
+/*
+CreateArgs function creates a GraphQl Object Type from the 'SalesInvoice'
+
+example of GraphQl Argument Object
+
+	map[string]*graphql.ArgumentConfig{
+		"No":					&graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)},
+		"Sell_to_Customer_No":	&graphql.ArgumentConfig{Type: graphql.String},
+		"Sell_to_Customer_Name":&graphql.ArgumentConfig{Type: graphql.String},
+		...
+	}
+
+Hint: arguments are used to create or update entities,
+some arguments are required and hence in the SalesInvoice type,
+tags can be noticed
+
+example of required fields
+
+	No	string `json:"No" required:"true"`
+
+and this will be translated to
+
+	"No":	&graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)},
+
+
+The returned GraphQl Object Type will be used as a part of the main query
+*/
 func CreateArgs() map[string]*graphql.ArgumentConfig {
 	return types.GenerateGraphQlArgs(SalesInvoice{}, nil)
 }
