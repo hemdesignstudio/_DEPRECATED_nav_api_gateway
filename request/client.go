1a62b64a47f52762e53893830181a3a00a507b3d
ahmednafies <ahmed.nafies@gmail.com>
2019-03-06 10:53:56 +0200

    requests documentation is added

--- request/client.go
+++ request/client.go
@@ -1,3 +1,12 @@
+// Copyright 2019 Hem Design Studio. All rights reserved.
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file.
+
+/*
+Package request implements a simple package for handling GraphQl fields.
+
+
+*/
 package request
 
 import (
@@ -9,6 +18,7 @@ import (
 	"net/url"
 )
 
+// headers contain request headers to Microsft Navision
 func headers(uri string, method string, body interface{}) *http.Request {
 	u, err := url.Parse(uri)
 	if err != nil {
@@ -24,14 +34,20 @@ func headers(uri string, method string, body interface{}) *http.Request {
 	}
 
 	req, err := http.NewRequest(method, u.String(), bytes.NewBuffer(_body))
+
+	// Basic authentication is added to header
 	req.SetBasicAuth(config.Username, config.Passwd)
+
+	// others header values are defined here
 	req.Header.Add("If-Match", "*")
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/json; odata.metadata=minimal")
+
 	return req
 
 }
 
+// clientRequest handles all requests to Microsft Navision
 func clientRequest(req *http.Request, method string) (int, interface{}, error) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -59,6 +75,7 @@ func clientRequest(req *http.Request, method string) (int, interface{}, error) {
 	return resp.StatusCode, respBody, nil
 }
 
+// request handles the http request and response to Microsoft Navision
 func request(uri string, method string, body interface{}) (int, interface{}, error) {
 	req := headers(uri, method, body)
 	return clientRequest(req, method)
--- request/service.go
+++ request/service.go
@@ -1,3 +1,7 @@
+// Copyright 2019 Hem Design Studio. All rights reserved.
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file.
+
 package request
 
 import (
@@ -5,6 +9,9 @@ import (
 	"github.com/hem-nav-gateway/errorhandler"
 )
 
+// GetAll handles getting all entities for a specified object types (customer, item ... etc)
+// takes fields --> fields to be returned from Navision
+// returns a list of requests object values
 func GetAll(endpoint string, fields, response interface{}) (interface{}, error) {
 	resValue := getAllEntities(endpoint, fields)
 	err := json.Unmarshal(resValue.([]byte), &response)
@@ -15,6 +22,10 @@ func GetAll(endpoint string, fields, response interface{}) (interface{}, error) 
 	return res["value"], nil
 }
 
+// Filter handles getting specific entities of specified object types (customer, item ... etc)
+// takes fields --> fields to be returned from Navision
+// takes args --> filter arguments
+// returns a list of filtered object values
 func Filter(endpoint string, fields, args, response interface{}) (interface{}, error) {
 	resValue, resError := filterByArgs(endpoint, fields, args)
 	if resError != nil {
@@ -32,6 +43,10 @@ func Filter(endpoint string, fields, args, response interface{}) (interface{}, e
 	return values, nil
 }
 
+// Create a specific entity of a specified object type (customer, item ... etc)
+// takes fields --> fields to be returned from Navision
+// takes args --> arguments are object values to be created
+// returns the created object with its return fields
 func Create(endpoint string, fields, args, response interface{}) (interface{}, error) {
 	body, _ := json.Marshal(args)
 	resValue, resError := createEntity(endpoint, fields, body)
@@ -45,6 +60,10 @@ func Create(endpoint string, fields, args, response interface{}) (interface{}, e
 	return response, nil
 }
 
+// Update a specific entity of a specified object type (customer, item ... etc)
+// takes fields --> fields to be returned from Navision
+// takes args --> arguments are object values to be updated
+// returns the created object with its return fields
 func Update(endpoint string, fields, args, docType, response interface{}) (interface{}, error) {
 
 	var resValue interface{}
@@ -77,6 +96,9 @@ func Update(endpoint string, fields, args, docType, response interface{}) (inter
 	return response, nil
 }
 
+// Delete a specific entity of a specified object type (customer, item ... etc)
+// takes args --> arguments used to get entity to be deleted
+// returns the response code
 func Delete(endpoint string, args map[string]interface{}, docType interface{}) (interface{}, error) {
 
 	var resCode int
