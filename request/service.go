b2eb6f5fe26332dc752a63a5dafeb2de6b208af6
ahmednafies <ahmed.nafies@gmail.com>
2019-03-06 11:07:34 +0200

    documentation is added to service

--- request/service.go
+++ request/service.go
@@ -71,16 +71,29 @@ func Update(endpoint string, fields, args, docType, response interface{}) (inter
 
 	body, _ := json.Marshal(args)
 	_args := args.(map[string]interface{})
+
 	if docType != nil {
 		docType := docType.(string)
+
+		// In Order to update SalesLines for example
+		// It is required to specify "Line_No", "Document_type", "Document_No"
+		// In this specific case "Document_No" acts as id
+		// this is related to how Microsoft Navision works
 		if lineNo, ok := _args["Line_No"]; ok {
 			id := _args["Document_No"].(string)
 			lineNo := lineNo.(int)
 			resValue, resError = updateEntitybyDocumentTypeAndIDAndLineNo(endpoint, id, docType, fields, lineNo, body)
+
+			// In order to update SalesOrder or SalesInvoice
+			// it is required to specify its "No" which acts as its id
+			// and "Document_type" which specifies if it is "Order" or "Invoice"
 		} else {
 			id := _args["No"].(string)
 			resValue, resError = updateEntitybyDocumentTypeAndID(endpoint, id, docType, fields, body)
 		}
+
+		// This is the case for most entities to be updated
+		// "No" which acts as id is all what it required to update an entity
 	} else {
 		id := _args["No"].(string)
 		resValue, resError = updateEntitybyId(endpoint, id, fields, body)
@@ -106,14 +119,26 @@ func Delete(endpoint string, args map[string]interface{}, docType interface{}) (
 
 	if docType != nil {
 		docType := docType.(string)
+
+		// In Order to delete and entity like "SalesLines" for example
+		// It is required to specify "Line_No", "Document_type", "Document_No"
+		// In this specific case "Document_No" acts as id
+		// this is related to how Microsoft Navision works
 		if lineNo, ok := args["Line_No"]; ok {
 			id := args["Document_No"].(string)
 			lineNo := lineNo.(int)
 			resCode, resError = deleteEntitybyDocumentTypeAndIDAndLineNo(endpoint, id, docType, lineNo)
+
+			// In order to delete SalesOrder or SalesInvoice
+			// it is required to specify its "No" which acts as its id
+			// and "Document_type" which specifies if it is "Order" or "Invoice"
 		} else {
 			id := args["No"].(string)
 			resCode, resError = deleteEntitybyDocumentTypeAndID(endpoint, id, docType)
 		}
+
+		// This is the case for most entities to be deleted
+		// "No" which acts as id is all what it required to delete an entity
 	} else {
 		id := args["No"].(string)
 		resCode, resError = deleteEntitybyId(endpoint, id)
