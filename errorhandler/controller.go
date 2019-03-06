8a6910f4accf03b786882fcbfb5de28027bbac12
ahmednafies <ahmed.nafies@gmail.com>
2019-03-06 09:29:56 +0200

    documentation is added to errorhandler

--- errorhandler/controller.go
+++ errorhandler/controller.go
@@ -1,3 +1,7 @@
+// Copyright 2019 Hem Design Studio. All rights reserved.
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file.
+
 package errorhandler
 
 import (
@@ -6,46 +10,79 @@ import (
 	"net/http"
 )
 
+// badRequest is invoked when Microsft Navision returns response code "400"
+// returns response code, Navision code and message to requester
 func badRequest(statusCode int, handler Handler) error {
 	return fmt.Errorf("status: %d, %s, %s", statusCode, handler.Content.Code, handler.Content.Message)
 }
 
+// errorMessageFromNav is invoked when the response code from Microsft Navision != 200
+// returns response code, Navision code and message to requester
 func errorMessageFromNav(statusCode int, handler Handler) error {
 	return fmt.Errorf("status: %d, %s, %s", statusCode, handler.Content.Code, handler.Content.Message)
 }
 
+// navInternalServerError is invoked when Microsft Navision returns response code "500"
+// returns response code, Navision code and message to requester
 func navInternalServerError(statusCode int, handler Handler) error {
 	return fmt.Errorf("status: %d, %s, Internal Server Error in Microsoft NAV", statusCode, handler.Message)
 }
 
+// ValueIsNotCorrect is invoked when requester, uses incorrect filter arguments
+// returns wrong arguments to the requester
 func ValueIsNotCorrect(args interface{}) error {
 	_args := args.(map[string]interface{})
 	return fmt.Errorf(" there are no entries for '%s' of value '%s' ", _args["key"], _args["value"])
 }
 
+// CouldNotUnmarshalData in invoked when The API Gateway
+// in unable to decode JSON from Navision
 func CouldNotUnmarshalData() error {
-	return fmt.Errorf("could not unmarshal data")
+	return fmt.Errorf("could not unmarshal data, NAV API Gateway internal server error")
 }
 
+/*
+errorOccurredButNotUnmarshalled is invoked when Microsoft Navision returns
+error which does not follow the normal format.
+
+Normal error response format:
+
+	'''
+	{
+		"error": {
+			"code": "BadRequest",
+			"message": "Could not find a property named 'hello' on type 'NAV.CustomerCardWS'."
+		}
+	}
+	'''
+
+However sometimes navision returns this:
+
+	'''
+	{
+		"Message": "An error has occurred."
+	}
+	'''
+
+ Or other formats and atm. this Gateway API handles only the first type
+*/
 func errorOccurredButNotUnmarshalled() error {
 	return fmt.Errorf("error occured, however Gateway API has failed to unmarshal the error response from NAV")
 }
 
+// unauthorized is invoked when Microsft Navision returns response code "401"
 func unauthorized() error {
 	return fmt.Errorf("NAV credentials in GateWay configuration is not correct")
 
 }
 
-func NotEnoughArguments() error {
-	return fmt.Errorf("please enter arguments, it is not permitted to create an empty invoice")
-
-}
-
+// CompanyDoesNotExist is invoked when wrong company is requested in URL path
 func CompanyDoesNotExist(name string) error {
 	return fmt.Errorf(fmt.Sprintf(" '/%s' does no exist, available options are '/test' ", name))
 
 }
 
+// Handle is the main function for handling all types of errors
 func Handle(statusCode int, errBody []byte) error {
 	handler := Handler{}
 	err := json.Unmarshal(errBody, &handler)
--- errorhandler/entity.go
+++ errorhandler/entity.go
@@ -1,3 +1,30 @@
+// Copyright 2019 Hem Design Studio. All rights reserved.
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file.
+
+/*
+Package errorhandler implements a simple package for handling errors related
+to Hem Nav API Gateway
+
+Handler type defined in this package is used to decode JSON coming from Microsft Navision In case of
+errors
+
+Example Error Response from Microsoft Navision:
+
+	'''
+	{
+		"error": {
+			"code": "BadRequest",
+			"message": "Could not find a property named 'hello' on type 'NAV.CustomerCardWS'."
+		}
+	}
+	'''
+
+Example way of decoding JSON error message
+	handler := Handler{}
+	err := json.Unmarshal(errorBody, &handler)
+
+*/
 package errorhandler
 
 type Handler struct {
