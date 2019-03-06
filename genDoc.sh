53f97b96371e5c6411addf0048db299fafd30d87
ahmednafies <ahmed.nafies@gmail.com>
2019-03-06 18:43:42 +0200

    godoc generation

--- genDoc.sh
+++ genDoc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 go run main.go
-graphdoc -e http://localhost:6789/graphql/v0.1.0/company=test -o ./doc
-graphql-markdown http://localhost:6789/graphql/v0.1.0/company=test > SCHEMA.md
+graphdoc -e http://localhost:6789/graphql/v0.1.0/test -o ./doc
+graphql-markdown http://localhost:6789/graphql/v0.1.0/test > SCHEMA.md
+
+godoc -http=localhost:6060 & wget -r -np -N -E -p -k http://localhost:6060/pkg/github.com/hem-nav-gateway/
