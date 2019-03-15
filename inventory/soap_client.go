09f931a6e7ffac5357323adcb2d2da58fcc940a6
ahmednafies <ahmed.nafies@gmail.com>
2019-03-15 12:54:36 +0200

    sll added to soap client

--- inventory/soap_client.go
+++ inventory/soap_client.go
@@ -3,21 +3,51 @@ package inventory
 import (
 	"bytes"
 	"crypto/tls"
+	"crypto/x509"
 	"encoding/xml"
 	"fmt"
 	"github.com/hem-nav-gateway/config"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 )
 
 const (
-	TYPE = "Codeunit"
+	TYPE    = "Codeunit"
+	CA_CERT = "./cert/CA.pem"
 )
 
 func formatUri(company string) string {
 
 	uri := fmt.Sprintf("%s/%s/%s%s", baseUrl, company, TYPE, endpoint)
 	return uri
+}
+
+func handleTls() *http.Transport {
+
+	rootCAs, _ := x509.SystemCertPool()
+	if rootCAs == nil {
+		rootCAs = x509.NewCertPool()
+	}
+
+	certs, err := ioutil.ReadFile(CA_CERT)
+	if err != nil {
+		log.Fatalf("Failed to append %q to RootCAs: %v", CA_CERT, err)
+	}
+
+	if ok := rootCAs.AppendCertsFromPEM(certs); !ok {
+		log.Println("No certs appended, using system certs only")
+	}
+
+	conf := &tls.Config{
+		InsecureSkipVerify: false,
+		RootCAs:            rootCAs,
+	}
+
+	tr := &http.Transport{TLSClientConfig: conf}
+
+	return tr
 }
 
 func soapRequest(obj *SoapObject) (interface{}, error) {
@@ -37,13 +67,8 @@ func soapRequest(obj *SoapObject) (interface{}, error) {
 	req.Header.Set("SOAPAction", SOAP_ACTION)
 	req.SetBasicAuth(config.Username, config.Passwd)
 
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		},
-	}
+	tr := handleTls()
+	client := &http.Client{Transport: tr}
 
 	res, err := client.Do(req)
 	if err != nil {
